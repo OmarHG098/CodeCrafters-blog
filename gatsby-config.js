fbff98eda8074558c0402c9c16edf832e1756662
Omar Hidalgo <omar_ehr@outlook.com>
2025-03-14 00:06:21 -0600

    ADD source filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,20 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [    
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `src`,
+      path: `${__dirname}/src`,
+    },
+  },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `posts`,
+      path: `${__dirname}/content/posts`,
+    },
+  },
+  ],
 }
